d0a25068cc73032f4706bbbf1d3b8a15ef65f0e5
joseph muli <jayjaymuli00@gmail.com>
2016-08-15 19:13:00 +0300

    prime list method, comments

--- toy_problems/prime_numbers.py
+++ toy_problems/prime_numbers.py
@@ -2,7 +2,13 @@
 Prime Numbers
 """
 
+"""
+prime checks whether a number is prime and returns either true or false
+"""
 
+"""
+prime_list appends all primes in a list and returns the list
+"""
 
 def prime(x):
 	if x < 2:
@@ -14,4 +20,19 @@ def prime(x):
 				return False
 		return True
 
-print prime(100)
+# print prime(100)
+
+
+def prime_list(b):
+	primes = []
+	for x in range(2,b):
+		if prime(x):
+			primes.append(x)
+			return primes
+		else:
+			print 'input a prime number'
+
+
+
+
+print prime_list(100)
