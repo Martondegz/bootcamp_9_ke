03e89679c8fe3801ab0c50d403f77f96f4bb9bc8
joseph muli <jayjaymuli00@gmail.com>
2016-08-17 23:28:20 +0300

    [string_reverse.py] Method update
    Simpler and compact method retained

--- andelabs/string_reverse.py
+++ andelabs/string_reverse.py
@@ -25,28 +25,3 @@ def reverse_string(string):
 
 
 
-def reverse_string(string):
-  
-  # first check for string length or Null
-  if len(string) == 0:
-    return None
-  
-  # empty list initialized to store elements of reversed strings
-  str_list = []
-  
-  # loop through string length and applying slicing, each element is reversed
-  for i in range(len(string)):
-    
-    # reversed elements are added to str_list
-    str_list.append(string[len(string)-i-1])
-  
-  # str_list convert to string and stored in variable str2
-  str2 = ''.join(str_list)
-  #print str2
-  
-  # check on whether reverse is even or like original string (palindrome)
-  if str2 == string:
-    return True
-
-  else: 
-    return str2
