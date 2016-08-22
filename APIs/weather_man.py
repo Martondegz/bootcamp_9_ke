4e7dd1cc47883b3b74eaf61b45e2e58716b028f5
joseph muli <jayjaymuli00@gmail.com>
2016-08-22 16:37:45 +0300

    weather updates GET only

--- APIs/weather_man.py
+++ APIs/weather_man.py
@@ -1,30 +1,33 @@
-import urllib2, urllib, json
+import json
 import urllib3
-
-
+from pprint import pprint
 
 
 def weather_man(city_name):
 	http = urllib3.PoolManager()
 	base_url = "http://api.openweathermap.org/data/2.5/weather"
 	consumer_key = '2bc3e79bb974a007818864813f53fd35'
-	city_name = city_name
+	
+	main = {}
+	# sys = {}
+	for city in city_name:
+		main_data = {}
+		# sys_data = {}
+		r = http.request('GET', base_url, 
+			fields={
+			'q': city_name,
+			'APPID': consumer_key,
+			'units':'metrics',
+			})
 
-	r = http.request('GET', base_url, 
-		fields={
-		'q': city_name,
-		'APPID': consumer_key,
-		'units':'metrics',
-		})
+		response = json.loads(r.data.decode('utf-8'))
+		main_data.update(response['main'])
+		# sys_data.update(response['sys'])
+		# sys[city] = sys_data
+		main[city] = main_data
 
-	response =json.loads(r.data.decode('utf-8'))
-	with open('weather.json', 'w') as f:
-		for resp in response:
-			f.write("{}\n".format(json.dumps(response)))
-			# json.dump(response, f)
-			# return True
-
-	# print response
+	pprint(main)
+	# pprint(sys)
 
 
-weather_man('Kampala')
+weather_man(['Kampala', 'Nairobi', 'Lagos'])
