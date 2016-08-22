6e460a80f195fe49f7bd660148fc74356e9ca650
joseph muli <jayjaymuli00@gmail.com>
2016-08-22 18:18:56 +0300

    new organized format

--- APIs/weather_man.py
+++ APIs/weather_man.py
@@ -5,25 +5,36 @@ from pprint import pprint
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 CONSUMER_KEY = '2bc3e79bb974a007818864813f53fd35'
 
+
 def weather_man(city_name):
 	http = urllib3.PoolManager()
-	main = {}
 	
+	header = 'city'.ljust(15)
+	header += 'humidity'.ljust(15)
+	header += 'temperature'.ljust(15)
+	header += 'description'.ljust(15)+'\n'
+	header += '='*55
+	print header
+
 	for city in city_name:
-		main_data = {}
 		
 		r = http.request('GET', BASE_URL, 
 			fields={
-			'q': city_name,
+			'q': city,
 			'APPID': CONSUMER_KEY,
-			'units':'metrics',
+			'units':'metric',
 			})
 
 		response = json.loads(r.data.decode('utf-8'))
-		main_data.update(response['main'])
-		main[city] = main_data
 
-	pprint(main)
+		output = response['name'].ljust(15)
+		output += str(response['main']['humidity']).ljust(15)
+		output += str(response['main']['temp']).ljust(15)
+		output += str(response['weather'][0]['description']).ljust(15)
+		
+		
+		print output 
+	
 
 
-weather_man(['Kampala', 'Nairobi', 'Lagos'])
+weather_man(['Kampala', 'Nairobi', 'Lagos', 'New York','Madrid'])
