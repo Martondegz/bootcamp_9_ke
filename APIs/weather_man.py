e39b90c86335f9514e8b3d30c4384a8bdc75382b
joseph muli <jayjaymuli00@gmail.com>
2016-08-22 16:45:27 +0300

    comments, constants

--- APIs/weather_man.py
+++ APIs/weather_man.py
@@ -2,32 +2,28 @@ import json
 import urllib3
 from pprint import pprint
 
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+CONSUMER_KEY = '2bc3e79bb974a007818864813f53fd35'
 
 def weather_man(city_name):
 	http = urllib3.PoolManager()
-	base_url = "http://api.openweathermap.org/data/2.5/weather"
-	consumer_key = '2bc3e79bb974a007818864813f53fd35'
-	
 	main = {}
-	# sys = {}
+	
 	for city in city_name:
 		main_data = {}
-		# sys_data = {}
-		r = http.request('GET', base_url, 
+		
+		r = http.request('GET', BASE_URL, 
 			fields={
 			'q': city_name,
-			'APPID': consumer_key,
+			'APPID': CONSUMER_KEY,
 			'units':'metrics',
 			})
 
 		response = json.loads(r.data.decode('utf-8'))
 		main_data.update(response['main'])
-		# sys_data.update(response['sys'])
-		# sys[city] = sys_data
 		main[city] = main_data
 
 	pprint(main)
-	# pprint(sys)
 
 
 weather_man(['Kampala', 'Nairobi', 'Lagos'])
